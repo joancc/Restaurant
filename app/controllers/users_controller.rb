b519238fcd856a82d530611a0cf9a2f8e1c5efb3
Joan <joancc@gmail.com>
2014-07-19 19:06:46 -0400

    Owner dashboard available when signed in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,5 @@
 class UsersController < ApplicationController
-  # def index
-  # end
-
-  # def login
-  #   #Login form
-  # end
-
-  # def new
-  #   @user = User.new
-  # end
-
-  # def create
-  #   @user = User.new(access_params)
-  #   if @user.save
-  #     flash[:notice] = "Sign up successful."
-  #     redirect_to(action: 'login', controller: 'access')
-  #   else
-  #     flash[:error] = @user.errors.full_messages
-  #     render('new')
-  #   end
-  # end
-
-  # private 
-
-  # def access_params
-  #   params.require(:user).permit(:email, :password, :name, :password_confirmation)
-  # end
+  def dashboard
+    @restaurants = Restaurant.where(user_id: current_user.id)
+  end
 end
